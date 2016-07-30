196e324f5f1f092c0e21f4c267cb831bea8e0f0c
tbalden <illespal@gmail.com>
2016-07-30 12:48:59 -0700

    adrenoboost: finetuning algorithm - scale it a bit down
    Values range from 0-3, 0 off, 3 most aggressive.
    Scales back nicely in 2d to 133mhz.
    Only scale up and count if MIN_VALUE crossed
    
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- drivers/devfreq/governor_msm_adreno_tz.c
+++ drivers/devfreq/governor_msm_adreno_tz.c
@@ -59,7 +59,8 @@ static DEFINE_SPINLOCK(suspend_lock);
 
 #define TAG "msm_adreno_tz: "
 
-static unsigned int adrenoboost = 10000;
+static unsigned int adrenoboost = 1;
+
 static u64 suspend_time;
 static u64 suspend_start;
 static unsigned long acc_total, acc_relative_busy;
@@ -96,7 +97,7 @@ static ssize_t adrenoboost_save(struct device *dev,
 {
 	int input;
 	sscanf(buf, "%d ", &input);
-	if (input < 0 || input > 50000) {
+	if (input < 0 || input > 3) {
 		adrenoboost = 0;
 	} else {
 		adrenoboost = input;
@@ -104,7 +105,6 @@ static ssize_t adrenoboost_save(struct device *dev,
 
 	return count;
 }
-
 
 static ssize_t gpu_load_show(struct device *dev,
 		struct device_attribute *attr,
@@ -383,7 +383,13 @@ static int tz_get_target_freq(struct devfreq *devfreq, unsigned long *freq)
 
 	*freq = stats.current_frequency;
 	priv->bin.total_time += stats.total_time;
-	priv->bin.busy_time += stats.busy_time;
+
+	// scale busy time up based on adrenoboost parameter, only if MIN_BUSY exceeded...
+	if ((unsigned int)(priv->bin.busy_time + stats.busy_time) >= MIN_BUSY) {
+		priv->bin.busy_time += stats.busy_time * (1 + (adrenoboost*3)/2);
+	} else {
+		priv->bin.busy_time += stats.busy_time;
+	}
 
 	if (stats.private_data)
 		context_count =  *((int *)stats.private_data);
@@ -421,9 +427,9 @@ static int tz_get_target_freq(struct devfreq *devfreq, unsigned long *freq)
 		scm_data[0] = level;
 		scm_data[1] = priv->bin.total_time;
 		if (refresh_rate > 60)
-			scm_data[2] = (priv->bin.busy_time * refresh_rate / 60) + (level * adrenoboost);
+			scm_data[2] = priv->bin.busy_time * refresh_rate / 60;
 		else
-			scm_data[2] = priv->bin.busy_time + (level * adrenoboost);
+			scm_data[2] = priv->bin.busy_time;
 		scm_data[3] = context_count;
 		__secure_tz_update_entry3(scm_data, sizeof(scm_data),
 					&val, sizeof(val), priv);
