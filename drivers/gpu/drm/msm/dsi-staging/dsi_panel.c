130be8d583086f0d680ee90aa46736e879bc3d28
GtrCraft <nickvanbokhorst@hotmail.com>
2021-01-23 19:37:55 +0100

    drivers: Remove klapse

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
@@ -27,10 +27,6 @@
 #include "dsi_panel_mi.h"
 #include "xiaomi_frame_stat.h"
 
-#ifdef CONFIG_KLAPSE
-#include <linux/klapse.h>
-#endif
-
 /**
  * topology is currently defined by a set of following 3 values:
  * 1. num of layer mixers
@@ -739,10 +735,6 @@ int dsi_panel_set_backlight(struct dsi_panel *panel, u32 bl_lvl)
 		pr_err("Backlight type(%d) not supported\n", bl->type);
 		rc = -ENOTSUPP;
 	}
-	
-#ifdef CONFIG_KLAPSE
-	set_rgb_slider(bl_lvl);
-#endif
 
 	return rc;
 }
