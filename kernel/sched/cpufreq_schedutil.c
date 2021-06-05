f05e0fa0e9b8113aadf5ae54e15897e43833299e
GtrCraft <nickvanbokhorst@hotmail.com>
2021-06-05 14:16:27 +0200

    Revert "cpufreq: schedutil: Expose default rate-limits as config options"
    This reverts commit 4e927ea8bccd1ad797f891f2ecbc09debd183c65.

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -1041,10 +1041,9 @@ static int sugov_init(struct cpufreq_policy *policy)
 	}
 
 	tunables->up_rate_limit_us =
-				CONFIG_SCHEDUTIL_UP_RATE_LIMIT;
+				cpufreq_policy_transition_delay_us(policy);
 	tunables->down_rate_limit_us =
-				CONFIG_SCHEDUTIL_DOWN_RATE_LIMIT;
-				
+				cpufreq_policy_transition_delay_us(policy);
 	tunables->hispeed_load = DEFAULT_HISPEED_LOAD;
 	tunables->hispeed_freq = 0;
 
