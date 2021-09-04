58c7ff02a7afda584d84ea519ebcc062e7df7318
GtrCraft <nickvanbokhorst@hotmail.com>
2021-09-04 14:25:15 +0200

    Optimus Drunk Surya v11.33

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@
 ## Copy this script inside the kernel directory
 KERNEL_DEFCONFIG=vendor/surya_defconfig
 ANYKERNEL3_DIR=$PWD/AnyKernel3/
-FINAL_KERNEL_ZIP=Optimus_Drunk_Surya_v11.32.zip
+FINAL_KERNEL_ZIP=Optimus_Drunk_Surya_v11.33.zip
 export PATH="$KERNELDIR/prebuilts/proton-clang/bin:${PATH}"
 export ARCH=arm64
 export SUBARCH=arm64
