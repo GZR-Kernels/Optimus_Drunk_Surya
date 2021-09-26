9d0208ee945c795e61047b29398ac1efa2110ba3
Unicote <arvogl874@gmail.com>
2021-09-26 11:10:56 +0300

    drivers: gpu: drm: msm: Add support for 48hz
    * The dynamic refresh rate will select 48hz in idle. This is necessary because at 30hz we can see small lags after idle.
    
    Signed-off-by: Unicote <arvogl874@gmail.com>
    Signed-off-by: chrisw444 <wandersonrodriguesf1@gmail.com>

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
@@ -1464,7 +1464,7 @@ static int dsi_panel_parse_dfps_caps(struct dsi_panel *panel)
 		goto error;
 	}
 
-	dfps_caps->dfps_list_len = 3;
+	dfps_caps->dfps_list_len = 4;
 
 	dfps_caps->dfps_list = kcalloc(dfps_caps->dfps_list_len, sizeof(u32),
 			GFP_KERNEL);
@@ -1472,6 +1472,7 @@ static int dsi_panel_parse_dfps_caps(struct dsi_panel *panel)
 	dfps_caps->dfps_list[0] = 120;
 	dfps_caps->dfps_list[1] = 90;
 	dfps_caps->dfps_list[2] = 60;
+	dfps_caps->dfps_list[3] = 48;
 
 	dfps_caps->dfps_support = true;
 
