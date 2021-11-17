3087e9e92dcaf02edf3f23e2cb740fa0f3eac04e
GtrCraft <nickvanbokhorst@hotmail.com>
2021-11-17 14:09:36 +0100

    Optimus Drunk Surya v11.38

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 
 KERNEL_DEFCONFIG=vendor/surya_defconfig
 ANYKERNEL3_DIR=$PWD/AnyKernel3/
-FINAL_KERNEL_ZIP=Optimus_Drunk_Surya_v11.37.zip
+FINAL_KERNEL_ZIP=Optimus_Drunk_Surya_v11.38.zip
 export ARCH=arm64
 
 # Speed up build process
@@ -27,7 +27,7 @@ echo -e "***********************************************$nocol"
 make $KERNEL_DEFCONFIG O=out
 make -j$(nproc --all) O=out \
                       ARCH=arm64 \
-                      CC=$KERNELDIR/prebuilts/clang-r433403/bin/clang \
+                      CC=$KERNELDIR/prebuilts/clang-r433403b/bin/clang \
                       CLANG_TRIPLE=aarch64-linux-gnu- \
                       CROSS_COMPILE=$KERNELDIR/prebuilts/aarch64-linux-android-4.9/bin/aarch64-linux-android- \
                       CROSS_COMPILE_ARM32=$KERNELDIR/prebuilts/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
