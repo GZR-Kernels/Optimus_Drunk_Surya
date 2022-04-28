a551c86f9c6e34dbb6f8e23a6399f6b6171c0040
GtrCraft <nickvanbokhorst@hotmail.com>
2022-04-28 09:04:17 +0200

    Optimus Drunk Surya v12.3

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 
 KERNEL_DEFCONFIG=vendor/surya_defconfig
 ANYKERNEL3_DIR=$PWD/AnyKernel3/
-FINAL_KERNEL_ZIP=Optimus_Drunk_Surya_v12.2.zip
+FINAL_KERNEL_ZIP=Optimus_Drunk_Surya_v12.3.zip
 export ARCH=arm64
 
 # Speed up build process
