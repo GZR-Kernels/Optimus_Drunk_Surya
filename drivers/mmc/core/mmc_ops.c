9ce8bddaf23548b76dd63e114e1cd506d0a27587
GtrCraft <nickvanbokhorst@hotmail.com>
2022-05-27 10:17:16 +0200

    Optimus Drunk Surya v12.8

--- drivers/mmc/core/mmc_ops.c
+++ drivers/mmc/core/mmc_ops.c
@@ -1109,7 +1109,6 @@ int mmc_flush_cache(struct mmc_card *card)
 			(card->ext_csd.cache_ctrl & 1) &&
 			(!(card->quirks & MMC_QUIRK_CACHE_DISABLE))) {
 		err = mmc_switch(card, EXT_CSD_CMD_SET_NORMAL,
-<<<<<<< HEAD
 				EXT_CSD_FLUSH_CACHE, 1, 0);
 		if (err == -ETIMEDOUT) {
 			pr_err("%s: cache flush timeout\n",
@@ -1121,11 +1120,6 @@ int mmc_flush_cache(struct mmc_card *card)
 				err = -ENODEV;
 			}
 		} else if (err) {
-=======
-				 EXT_CSD_FLUSH_CACHE, 1,
-				 MMC_CACHE_FLUSH_TIMEOUT_MS);
-		if (err)
->>>>>>> 3bf624404a1a02784205e5bf3a80681663c3be0d
 			pr_err("%s: cache flush error %d\n",
 					mmc_hostname(card->host), err);
 		}
